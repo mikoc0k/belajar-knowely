cdf0c810515ef579d16323253c5f5f9dd3226cbf
mikoc0k <uciharis@hotmail.com>
2025-06-05 17:48:37 +0700

    menambah fungsi untuk cek cek char tertentu didalam sebuah string

--- replaceAll.js
+++ replaceAll.js
@@ -3,3 +3,9 @@ return input.replaceAll("-"," ");
 }
 
 console.log(restoreSpaces("jembut-ireng-mulyono"));
+
+function isSubstring(phrase, part) {
+  return phrase.includes(part);
+}
+
+console.log(isSubstring("mulyono anak haram konstitusi", "ono"));
