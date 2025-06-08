4e157d8fa4a29281e601f9982101007f98a506a5
mikoc0k <uciharis@hotmail.com>
2025-06-08 12:03:39 +0700

    tambahan fungsi average dan cari nilai min

--- workingWithArrays.js
+++ workingWithArrays.js
@@ -26,3 +26,42 @@ function getSUM(prices) {
     }
     return `jumlah prices (for of) adalah : ${total}`;
     }
+
+
+// itung rerata
+// agar aman dari array kosong, kita buatkan if nya
+
+let jembod = [3,6,9];
+let jembit = [];
+function getAverage(values) {
+    if (values.length === 0) {
+        return 0;
+    }
+    let total = 0;
+    for (let value of values) {
+        total += value;
+    }
+    return total / values.length;
+}
+
+const average = getAverage(jembod);
+console.log(average);
+
+const aver= getAverage(jembit);
+console.log(aver);
+
+// max min value
+
+let jembrol = [564,31,21,3]
+function getMin(values) {
+    let min = values[0];
+    for (let value of values) {
+        if (value < min) {
+            min = value;
+        }
+    }
+    return min;
+}
+
+const min = getMin(jembrol);
+console.log(`nilai terkecil adalah ${min}`);
